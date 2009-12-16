c3e4969f5295b1187bd4fb29beda38b75597a20e
Thomas <thomas@icdesign.com.au>
2009-12-16 19:34:53 +1100

    Fixed failing spec for contact spec

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -3,7 +3,9 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Contact do
   before(:each) do
     @valid_attributes = {
-      
+      :name => "Thomas",
+      :email => "anathematic@github.com",
+      :message => "Please note that email address isn't my email address."
     }
   end
 
